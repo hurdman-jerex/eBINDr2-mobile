7c546d4e5e404bf00c0fe98379e2c9a60165d45f
Jerex Salingujay <skyguyverph@gmail.com>
2017-03-16 03:26:23 +0800

    Implemented Mergecode Base Edit Forms.

---
+++ business/website/edit.php
@@ -0,0 +1,29 @@
+<? include "../../templates/header.html"; ?>
+<? include "../../templates/nav-bar.html"; ?>
+
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span12">
+
+                <h2>Edit Web Address</h2>
+                <iframe id="reportIframe" class="mochaIframe" src="" marginwidth="0" marginheight="0" scrolling="auto" style="height: 565px; width: 98%;" frameborder="0"></iframe>
+
+            </div>
+        </div>
+    </div>
+
+    <script>jQuery.noConflict();</script>
+    <script src="/m/assets/js/mootools/core-1.2.4.js"></script>
+    <script src="/m/assets/js/mootools/more-1.2.4.2.js"></script>
+
+    <script src="/m/assets/js/ebindr.js.php?flex"></script>
+
+<? $_menuPage='/m/editr/e2m lite button bu/?ebindr2&bid='.$_SESSION['bid'].'&key1='.$_SESSION['bid'].'&key2='.$_GET['wid']; ?>
+    <script type="text/javascript">
+        jQuery( document ).ready(function() {
+            ebindr.initialize();
+            ebindr.initIFrame( 'reportIframe', '<?=$_menuPage?>' );
+        });
+    </script>
+
+<? include "../../templates/footer.html"; ?>
